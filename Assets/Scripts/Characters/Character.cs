3265ddb89c45480d386dfe247d9058a095c0e6f0
Ben Hicks <bh.benhicks@gmail.com>
2018-01-30 23:34:37 -0500

    Initial Commit
    Contains the base unity files

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public Material matChr;
+	public string sName;
+
+	void InitMaterial(){
+		string sMatPath = "Materials/Characters/mat" + sName;
+		matChr = Resources.Load(sMatPath, typeof(Material)) as Material;
+		GetComponent<Renderer> ().material = matChr;
+	}
+
+	void Initialize(){
+		InitMaterial ();
+	}
+
+	public Character(){
+		
+	}
+
+	// Use this for initialization
+	public void Start () {
+		Debug.Log ("Base Character Starting");
+		Initialize ();
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Characters/ChrSkelCowboy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChrSkelCowboy : Character {
+
+	ChrSkelCowboy():base(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		sName = "SkelCowboy";
+		Debug.Log ("I'm " + sName);
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
