46b8ff1647d6b6fa5018d0183a9b0f160efbc16d
ConnorReedMacLeod <n01176813@humbermail.ca>
2018-02-19 18:20:22 -0500

    Added comments to Mana code
    Excessively so

--- Assets/Scripts/Model/Mana.cs
+++ Assets/Scripts/Model/Mana.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿//BEN HICKS
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,24 +9,25 @@ public class Mana : Subject {
 		PHYSICAL, MENTAL, ENERGY, BLOOD, EFFORT
 	};
 
-	public static int nManaTypes = 5;
+	public static int nManaTypes = 5; //Number of mana types (PHYSICAL, MENTAL, ENERGY, BLOOD, EFFORT)
 
-	public int[] arMana;
-	public int[] arManaPool;
-	//for keeping track of the qorder in which mana was added
+	public int[] arMana;        //Player's mana, total
+	public int[] arManaPool;    //Player's mana in mana pool
+
+	//Tracks the order in which mana was added to the mana pool
 	public LinkedList<MANATYPE> qManaPool;
 
-	//For adding just one of a single type of mana
+	//For adding one mana of one type to player's total mana
 	public void AddMana(MANATYPE type){
 		AddMana (type, 1);
 	}
 
-	//For adding just a single type of mana
+	//For adding any number of mana of one type to player's total mana
 	public void AddMana(MANATYPE type, int nAmount){
 		arMana [(int)type] += nAmount;
 	}
 
-	//If it's more convenient to add a bunch of mana
+	//For adding any number of mana of any number of types to player's total mana, using an array of MANATYPEs
 	public void AddMana(int [] _arMana){
 		Debug.Assert (_arMana.Length == nManaTypes || _arMana.Length == nManaTypes - 1);
 
@@ -34,19 +36,22 @@ public class Mana : Subject {
 		}
 	}
 
+    //For adding one mana of one type to player's mana pool
 	public bool AddToPool(MANATYPE type){
 		return AddToPool (type, 1);
 	}
 
-	public bool AddToPool(MANATYPE type, int nAmount){
+    //For adding any number of mana of one type to player's mana pool
+    public bool AddToPool(MANATYPE type, int nAmount){
 
-		if (arMana [(int)type] < nAmount) {
+        //Checks if the player has enough mana outside the mana pool
+        if (arMana [(int)type] < nAmount) {
 			Debug.Log ("Not enough mana");
 			return false;
 		}
 
-		//We know we have enough mana
-		for (int i = 0; i < nAmount; i++) {
+        //Removes mana from outside the mana pool, placing it in the mana pool
+        for (int i = 0; i < nAmount; i++) {
 			arMana [(int)type]--;
 			arManaPool [(int)type]++;
 			qManaPool.AddLast (type);
@@ -55,19 +60,22 @@ public class Mana : Subject {
 		return true;
 	}
 
+    //For removing one mana of one type from the player's mana pool
 	public bool RemoveFromPool(MANATYPE type){
 		return RemoveFromPool (type, 1);
 	}
 
-	public bool RemoveFromPool(MANATYPE type, int nAmount){
+    //For removing any number of mana of one type from the player's mana pool
+    public bool RemoveFromPool(MANATYPE type, int nAmount){
 
-		if (arManaPool [(int)type] < nAmount) {
+        //Checks if the player has enough mana in the mana pool
+        if (arManaPool [(int)type] < nAmount) {
 			Debug.Log ("Not enough mana in pool");
 			return false;
 		}
 
-		//We know we have enough mana
-		for (int i = 0; i < nAmount; i++) {
+        //Removes mana from the mana pool, placing it outside the mana pool
+        for (int i = 0; i < nAmount; i++) {
 			arManaPool [(int)type]--;
 			arMana [(int)type]++;
 			qManaPool.Remove (type);
@@ -76,49 +84,63 @@ public class Mana : Subject {
 		return true;
 	}
 
+    //Checks to see if the player has enough mana total and in their mana pool to pay for a given cost
+    public bool HasMana(int[] arCost)
+    {
+        //Checks that the given cost contains only up to 5 mana types
+        Debug.Assert(arCost.Length == nManaTypes || arCost.Length == nManaTypes - 1);
 
-	public bool SpendMana(MANATYPE type){
+        int nTotalMana = 0;
+        int nTotalCost = 0;
+
+        //WARNING:: This assumes that effort can be paid with any
+        //          type of mana and that it is always in the last position in the array
+        for (int i = 0; i < arCost.Length; i++)
+        {
+            nTotalMana += arManaPool[i];
+            nTotalCost += arCost[i];
+
+            //For mana type [i], checks if the player has enough to cover the cost
+            //Effort mana, or mana type [5], will often fail this check, as it is always 0
+            if (arManaPool[i] < arCost[i])
+            {
+                //After all other costs are paid, leftover mana pool mana is used to pay for effort
+                if (i == (int)MANATYPE.EFFORT && nTotalMana >= nTotalCost)
+                {
+                    Debug.Log("Can pay effort with other types of mana");
+                    break;
+                }
+
+                Debug.Log("Can't pay this mana cost");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //For spending one mana of one type
+    public bool SpendMana(MANATYPE type){
 		return SpendMana (type, 1);
 	}
 
+    //For spending any number of mana of one type
 	public bool SpendMana(MANATYPE type, int nAmount){
-		int [] cost = new int[nManaTypes];
-		cost [(int)type] = nAmount;
-		return SpendMana (cost);
+		int [] arCost = new int[nManaTypes];
+		arCost [(int)type] = nAmount;
+		return SpendMana (arCost);
 	}
 
-	public bool HasMana(int [] arCost){
-		Debug.Assert (arCost.Length == nManaTypes || arCost.Length == nManaTypes - 1);
-
-		int nTotalMana = 0;
-		int nTotalCost = 0;
-
-
-		//WARNING:: This assume that effort can be paid with any
-		//          type of mana and that it is always in the last position in the array
-		for (int i = 0; i < arCost.Length; i++) {
-			nTotalMana += arManaPool [i];
-			nTotalCost += arCost [i];
-			if(arManaPool[i] < arCost[i]){
-				if (i == (int)MANATYPE.EFFORT && nTotalMana >= nTotalCost) {
-					Debug.Log ("Can pay effort with other types of mana");
-					break;
-				}
-				Debug.Log ("Can't pay this mana cost");
-				return false;
-			}
-		}
-
-		return true;
-	}
-
+    //For spending any number of mana of any type
 	public bool SpendMana(int [] arCost){
 		Debug.Assert (arCost.Length == nManaTypes || arCost.Length == nManaTypes - 1);
 
-		if (!HasMana (arCost))
-			return false;
+        //Uses HasMana function to check if player has enough mana to pay the given mana cost
+        if (!HasMana(arCost)) {
+            return false;
+        }
 
-		//We know we have enough mana to pay for this
+        //Removes mana to pay for cost
 		for (int i = 0; i < arCost.Length; i++) {
 			for (int j = 0; j < arCost [i]; j++) {
 				if (arManaPool [i] > 0) {
