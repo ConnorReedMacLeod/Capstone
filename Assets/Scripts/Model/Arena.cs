d5b4083aef2097f2a5a9ebd850d4e3264071d186
Ben-Hicks <bh.benhicks@gmail.com>
2018-04-15 22:46:18 -0400

    Characters Spawning in a Reasonable Place Now
    Initial Position for placing character set up, as well as a loose unit system.

--- Assets/Scripts/Model/Arena.cs
+++ Assets/Scripts/Model/Arena.cs
@@ -9,4 +9,77 @@ using UnityEngine;
 [RequireComponent (typeof(ViewArena))]
 public class Arena : Subject{
 
+	bool bStarted;
+
+	public float fArenaWidth;
+	public float fArenaHeight;
+
+	public int nWidth;
+	public int nHeight;
+	public float fUnit;
+
+	public float fStartPosX;
+	public float[] arfStartingPosY;
+
+	public void PlaceUnit(Chr chr, float x, float y){
+
+		if (x - chr.fRad < -(nWidth / 2)) {
+			Debug.LogError ("ERROR!  Trying to place this Chr too far left!");
+		}
+		if (x + chr.fRad > (nWidth / 2)) {
+			Debug.LogError ("ERROR!  Trying to place this Chr too far right!");
+		}
+		if (y - chr.fRad < -(nHeight / 2)) {
+			Debug.LogError ("ERROR!  Trying to place this Chr too far up!");
+		}
+		if (y + chr.fRad > (nHeight / 2)) {
+			Debug.LogError ("ERROR!  Trying to place this Chr too far down!");
+		}
+
+		chr.SetPosition (x, y);
+	}
+
+	public void InitPlaceUnit(Chr chr){
+		if (chr.plyrOwner.id == 0) {
+			PlaceUnit (chr, -fStartPosX * fArenaWidth, arfStartingPosY [chr.id] * fArenaHeight);
+		} else {
+			PlaceUnit (chr, fStartPosX * fArenaWidth, arfStartingPosY [chr.id] * fArenaHeight);
+		}
+	}
+
+	public void InitArenaSize(){
+
+		//Set the starting positions (as a percentage of the total arena size);
+		fStartPosX = 0.3f;
+		arfStartingPosY = new float[]{-0.25f, 0.0f, 0.25f};
+
+		fArenaWidth = transform.localScale.x;
+		fArenaHeight = transform.localScale.y;
+
+		fUnit = Chr.arfSize [(int)Chr.SIZE.MEDIUM];
+
+		float fRem = (fArenaWidth / fUnit) % 1.0f;
+		if (Mathf.Abs (fRem * (1.0f - fRem)) > 0.1f) {
+			Debug.LogError ("ALERT! Arena's width is not a multiple of the unit size!");
+		}
+		nWidth = (int)(fArenaWidth / fUnit);
+
+		fRem = (fArenaHeight / fUnit) % 1.0f;
+		if (Mathf.Abs (fRem * (1.0f - fRem)) > 0.1f) {
+			Debug.LogError ("ALERT! Arena's height is not a multiple of the unit size!");
+		}
+		nHeight = (int)(fArenaHeight / fUnit);
+	}
+
+	public void Start(){
+
+		if (bStarted == false) {
+			bStarted = true;
+
+			InitArenaSize ();
+
+		}
+	}
+
+
 }
--- Assets/Scripts/View/ViewArena.cs
+++ Assets/Scripts/View/ViewArena.cs
@@ -10,6 +10,7 @@ public class ViewArena : Observer {
 	public float fWidth;                //The Arena's width
 	public float fHeight;               //The Arena's height
 
+
 	bool bStarted;
 
     //Gets the Arena view's dimensions
@@ -24,6 +25,7 @@ public class ViewArena : Observer {
 			Vector3 size = GetComponent<Renderer> ().bounds.size;
 			fWidth = size.x;
 			fHeight = size.y;
+
 		}
     }
 
@@ -35,10 +37,14 @@ public class ViewArena : Observer {
 		mod.Subscribe (this);
 	}
 		
-    //Gets the mouse posititon within the Arena
+    //Gets the mouse position within the Arena
+	//WARNING:: This requires the arena to be centered at 0,0 in worldspace
 	public Vector3 GetArenaPos(Vector3 pos){
+
 		Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
 		//return new Vector3 (worldPos.x / fWidth, worldPos.y / fHeight, worldPos.z);//TODO:: figure out a proper z coord
+
+		Debug.Log("Click at " + new Vector3 (worldPos.x, worldPos.y, 0));
 		return new Vector3 (worldPos.x, worldPos.y, worldPos.z);
 	}
 
