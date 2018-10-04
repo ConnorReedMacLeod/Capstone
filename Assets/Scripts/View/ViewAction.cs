5692f7ea6c78f3fa777fe394d08e5b2ba4b29e57
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2018-10-04 18:12:07 -0400

    Adding Ability Buttons
    Still need to have buttons update when character is selected - need a controller for it

--- Assets/Scripts/View/ViewAction.cs
+++ Assets/Scripts/View/ViewAction.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // TODO:: Consider if not making this an observer is okay
 [RequireComponent (typeof(MouseHandler))]
-public class ViewAction : MonoBehaviour {
+public class ViewAction : Observer {
 
-	public int id;                              //The action's unique identifier
+    bool bStarted;                          //Confirms the Start() method has executed
+
+    public int id;                              //The action's unique identifier
 	public Action mod;                      		//The action's model
 	public MouseHandler mousehandler;
 
-    //Sets the ActionWheel segment's material
-	public void SetActionMaterial (string _sName){
-		string sMatPath = "Materials/Actions/matAction" + _sName;
-		Material matChr = Resources.Load(sMatPath, typeof(Material)) as Material;
-		GetComponent<Renderer> ().material = matChr;
-	}
+    //Textfields to display information
+    public Text txtName;
+    public Text txtType;
+    public Text txtRecharge;
+    public Text txtCooldown;
+    public Text txtRemaining;
 
     //Let the Action button know which character and id it's representing
 	public void SetModel (Action _mod){
 		mod = _mod;
-	}
+        DisplayAll();
+    }
 
 	public void Start(){
-		InitMouseHandler ();
+        if (bStarted == false)
+        {
+            bStarted = true;
+            Init();
+            InitMouseHandler();
+        }
 	}
 
 	public void InitMouseHandler(){
@@ -46,4 +55,116 @@ public class ViewAction : MonoBehaviour {
 	public void OnMouseExit(){
 		Controller.Get ().NotifyObs (Notification.ActStopHover, this, id);
 	}
+
+
+    override public void UpdateObs(string eventType, Object target, params object[] args)
+    {
+
+        switch (eventType)
+        {
+            //TODO:: Consider adding in field-specific update types if only one field needs updating
+
+            case Notification.ActionUpdate:
+                DisplayAll();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    public void DisplayName(){
+        if (mod == null){
+            txtName.text = "";
+        } else {
+            txtName.text = mod.sName;
+        }
+    }
+
+    public void DisplayCost() {
+        //TODO: THIS FUNCTION
+    }
+
+    public void DisplayType() {
+        if (mod == null) {
+            txtType.text = "";
+        } else {
+            txtType.text = "[" + mod.type.ToString() + "]";
+        }
+    }
+
+    public void DisplayRecharge() {
+        if (mod == null) {
+            txtRecharge.text = "";
+        } else {
+            txtRecharge.text = mod.nRecharge.ToString();
+        }
+    }
+
+    public void DisplayCooldown() {
+        if (mod == null) {
+            txtCooldown.text = "";
+        } else {
+            txtCooldown.text = mod.nCd.ToString();
+        }
+    }
+
+    public void DisplayRemaining() {
+        if (mod == null) {
+            txtRemaining.text = "";
+        } else {
+            //txtRemaining.text = "CD: " + mod.nCd.ToString();
+            //TODO:  THIS PART OF THE FUNCTION
+        }
+    }
+
+    public void DisplayAll() {
+        DisplayName();
+        DisplayCost();
+        DisplayType();
+        DisplayRecharge();
+        DisplayCooldown();
+        DisplayRemaining();
+    }
+
+    //Variable initialization
+    public void Init()
+    {
+        Text[] arTextComponents = GetComponentsInChildren<Text>();
+
+        for (int i = 0; i < arTextComponents.Length; i++)
+        {
+
+            switch (arTextComponents[i].name)
+            {
+                case "txtName":
+                    txtName = arTextComponents[i];
+                    break;
+
+                case "txtType":
+                    txtType = arTextComponents[i];
+                    break;
+
+                case "txtRecharge":
+                    txtRecharge = arTextComponents[i];
+                    break;
+
+                case "txtCooldown":
+                    txtCooldown = arTextComponents[i];
+                    break;
+
+                case "txtRemaining":
+                    txtRemaining = arTextComponents[i];
+                    break;
+
+                default:
+                    Debug.LogError("ERROR! Unrecognized Text component in ViewAction");
+                    break;
+            }
+        }
+
+        UpdateObs(Notification.InfoActionUpdate, null);
+    }
+
 }
