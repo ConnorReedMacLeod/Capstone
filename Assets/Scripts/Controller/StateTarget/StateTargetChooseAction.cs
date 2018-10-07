f0db332613a30d92fb2c24dd396de36319230a46
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2018-10-07 15:02:10 -0400

    some debug statements

--- Assets/Scripts/Controller/StateTarget/StateTargetChooseAction.cs
+++ Assets/Scripts/Controller/StateTarget/StateTargetChooseAction.cs
@@ -8,6 +8,7 @@ public class StateTargetChooseAction : StateTarget {
 
 		switch (eventType) {
 		case Notification.ClickAct:
+                Debug.Log("Selecting a new Action");
 			// When we've clicked an action, use that action
             
 			contTarg.selected.Targetting ();
@@ -20,14 +21,16 @@ public class StateTargetChooseAction : StateTarget {
 			break;
 		
 		case Notification.ClickArena:
-			// Then we've just clicked somewhere in the arena, so we can deselect the current Char
+                // Then we've just clicked somewhere in the arena, so we can deselect the current Char
+                Debug.Log("Clicked Arena, so stopping selection");
 
 			contTarg.SetState (new StateTargetIdle (contTarg));
 
 			break;
 
 		case Notification.ClickChr:
-			// Then we've clicked on a different character, so select them
+                // Then we've clicked on a different character, so select them
+                Debug.Log("Switching to select a new character instead");
 
 			contTarg.selected = ((ViewChr)target).mod;
 
