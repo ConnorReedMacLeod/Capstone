4504a06f58bf969e1ccf0582afe3af8e326a0ef9
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2018-11-10 15:55:08 -0500

    clause + executable classes added
    Need to change actions yet and build the controller for ability engine stuff

---
+++ Assets/Scripts/Model/AbilityEngine/Executables/ExecDealDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Can create executables like ...= new ExecDealDamage(){chrTarget = ..., nDamage = ...};
+
+public class ExecDealDamage : Executable {
+
+    public Chr chrTarget;
+    public int nDamage;
+
+	public override void Execute() {
+
+        chrTarget.ChangeHealth(-nDamage);
+
+        base.Execute();
+    }
+
+}
