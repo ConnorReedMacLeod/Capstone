b89bae43cd929c9df17e59ec981950d3328b1c47
Ben Hicks <bh.benhicks@gmail.com>
2018-11-23 19:51:18 -0500

    Added Some Trigger Subjects in Comments
    Uncomment them and finish adding those areas to each Executable

--- Assets/Scripts/Model/AbilityEngine/Executables/Executable.cs
+++ Assets/Scripts/Model/AbilityEngine/Executables/Executable.cs
@@ -8,6 +8,8 @@ public abstract class Executable {
     public string sLabel;
     public float fDelay;
 
+    //public abstract Subject GetPreTrigger();
+    //public abstract Subject GetPostTrigger();
 
     public virtual void Execute() {
 
