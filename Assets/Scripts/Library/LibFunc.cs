d13db7cc901e7c6edd5e49e4e44555202bb9d3ae
Ben Hicks <bh.benhicks@gmail.com>
2018-11-27 17:22:15 -0500

    Added some effects - Updating Soul effects
    still need to finish SoulBurning

---
+++ Assets/Scripts/Library/LibFunc.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LibFunc {
+
+    public delegate T Get <T>  ();
+
+    //Can be used to fix the return value, so that we always return
+    //the value of toReturn AS IT IS NOW, even if the source of toReturn changes later
+    public static Get<T> ReturnSnapShot<T>(T toReturn) {
+        return () => { return toReturn; };
+    }
+	
+}
--- Assets/Scripts/Model/Match.cs
+++ Assets/Scripts/Model/Match.cs
@@ -23,6 +23,9 @@ public class Match : MonoBehaviour {
 
 	public static Match instance;
 
+    
+    public delegate int del();
+    public delegate del curried(int i);
 
 	public static Match Get (){
 		if (instance == null) {
@@ -158,5 +161,29 @@ public class Match : MonoBehaviour {
 
         InitAllBlockers();
 
-	}
+
+        //BEGIN TESTING SECTION
+
+        curried curry = (int arg) => { return () => arg; };
+
+        int x = 3;
+
+        Debug.Log("x starts as " + x);
+
+        del fUncurried = () => { return x; };
+        LibFunc.Get<int> fCurried = LibFunc.ReturnSnapShot<int>(x);
+
+        Debug.Log("fUncurried: " + fUncurried());
+        Debug.Log("fCurried: " + fCurried());
+
+        x = 4;
+
+        Debug.Log("x has changed to " + x);
+
+        Debug.Log("fUncurried: " + fUncurried());
+        Debug.Log("fCurried: " + fCurried());
+
+        //END TESTING SECTION
+
+    }
 }
