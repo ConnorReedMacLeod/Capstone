4c0932f124f89498966460aa1ca969ecc14fa9c9
Ben Hicks <bh.benhicks@gmail.com>
2019-01-05 20:37:12 -0500

    Saiko and Rayne Added
    Most of their abilities are done

---
+++ Assets/Scripts/Model/AbilityEngine/Actions/Saiko/ActionStickyBomb.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionStickyBomb : Action {
+
+    public int nInitialDamage;
+
+    public ActionStickyBomb(Chr _chrOwner) : base(1, _chrOwner) {//number of target arguments
+
+        //We don't need to target anything, since we always deal damage to everyone
+        arArgs[0] = new TargetArgChr((own, tar) => true); 
+
+        sName = "StickyBomb";
+        type = ActionType.ACTIVE;
+
+        //Physical, Mental, Energy, Blood, Effort
+        parCost = new Property<int[]>(new int[] { 0, 1, 0, 0, 1 });
+
+        nCd = 6;
+        nFatigue = 3;
+        nActionCost = 1;
+
+        sDescription = "Deal 5 damage to target character, then deal 30 damage to that character at the end of turn";
+
+        nInitialDamage = 5;
+
+        SetArgOwners();
+    }
+
+    override public void Execute() {
+        //Cast the first target to be a character
+        Chr chrTarget = ((TargetArgChr)arArgs[0]).chrTar;
+
+        stackClauses.Push(new Clause() {
+            fExecute = () => {
+
+                //Deal initial damage to the target
+                Debug.Log("This StickyBomb Clause put an ExecDamage on the stack");
+                Damage dmgToDeal = new Damage(chrSource, chrTarget, nInitialDamage);
+
+                ContAbilityEngine.Get().AddExec(new ExecDealDamage() {
+                    chrSource = this.chrSource,
+                    chrTarget = chrTarget,
+                    dmg = dmgToDeal,
+
+                    fDelay = 1.0f,
+                    sLabel = chrTarget.sName + " is stuck with a bomb"
+                });
+
+                //Apply the stickybomb Soul effect to the target
+                Debug.Log("The same Clause put an ExecApplySoul on the stack");
+                ContAbilityEngine.Get().AddExec(new ExecApplySoul() {
+                    chrSource = this.chrSource,
+                    chrTarget = chrTarget,
+
+                    funcCreateSoul = (Chr _chrSource, Chr _chrTarget) => {
+                        return new SoulStickyBomb(_chrSource, _chrTarget);
+                    }
+
+                });
+            }
+        });
+
+
+        //NOTE:: Every Execute extension should begin with a typecast and end with a base.Execute call;
+        base.Execute();
+    }
+
+}
---
+++ Assets/Scripts/Model/Soul/Rayne/SoulCloudCushion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulCloudCushion : Soul {
+   
+    public int nDefenseBuff;
+    
+    public SoulChangeDefense soulChangeDefense;
+
+    public SoulCloudCushion(Chr _chrSource, Chr _chrTarget) : base(_chrSource, _chrTarget) {
+
+        sName = "CloudCushion";
+
+        nDefenseBuff = 25;
+
+        bVisible = true;
+        bDuration = true;
+        pnMaxDuration = new Property<int>(4);
+
+    }
+
+
+    public override void funcOnApplication() {
+
+        //Make a Permanent SoulChangeDefense, and save a reference to it, so it can be removed later
+        soulChangeDefense = new SoulChangeDefense(chrSource, chrTarget, nDefenseBuff);
+        chrTarget.soulContainer.ApplySoul(soulChangeDefense);
+
+        Debug.Log(sName + " has been applied");
+    }
+
+    public override void funcOnRemoval() {
+        chrTarget.soulContainer.RemoveSoul(soulChangeDefense);
+
+        Debug.Log(sName + " has been removed");
+
+    }
+
+    public override void funcOnExpiration() {
+        Debug.Log(sName + " has expired");
+    }
+}
