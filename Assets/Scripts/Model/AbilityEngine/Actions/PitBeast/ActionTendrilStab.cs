7cd86489c4c245db71b79062c32f372f25863114
Ben Hicks <bh.benhicks@gmail.com>
2019-01-23 13:16:17 -0500

    added healing class
    not working yet though

--- Assets/Scripts/Model/AbilityEngine/Actions/PitBeast/ActionTendrilStab.cs
+++ Assets/Scripts/Model/AbilityEngine/Actions/PitBeast/ActionTendrilStab.cs
@@ -4,49 +4,55 @@ using UnityEngine;
 
 public class ActionTendrilStab : Action {
 
+   //TODO:: Give all damaging actions an instance of damage to store and fill in with a target
+   //         when the damage is actually being applied - can easilly Get() the damage value accurately
+   //         when needed, even before dealing damage
+    public int nBaseDmg;
+
     public ActionTendrilStab(Chr _chrOwner) : base(0, _chrOwner) {//number of target arguments
 
-    //Since the base constructor initializes this array, we can start filling it
-    //arArgs[0] = new TargetArgChr((own, tar) => own.plyrOwner != tar.plyrOwner); we don't have any targets
+        //Since the base constructor initializes this array, we can start filling it
+        //arArgs[0] = new TargetArgChr((own, tar) => own.plyrOwner != tar.plyrOwner); we don't have any targets
 
-    sName = "TendrilStab";
-    type = new TypeActive(this);
+        sName = "TendrilStab";
+        type = new TypeActive(this);
 
-    //Physical, Mental, Energy, Blood, Effort
-    parCost = new Property<int[]>(new int[] { 1, 0, 0, 0, 0 });
+        //Physical, Mental, Energy, Blood, Effort
+        parCost = new Property<int[]>(new int[] { 1, 0, 0, 0, 0 });
 
-    nCd = 6;
-    nFatigue = 3;
-    nActionCost = 1;
+        nCd = 6;
+        nFatigue = 3;
+        nActionCost = 1;
+        nBaseDmg = 25;
 
-    sDescription = "Deal 25 [PIERCING] damage to the enemy blocker";
+        sDescription = "Deal 25 [PIERCING] damage to the enemy blocker";
 
-    SetArgOwners();
-}
+        SetArgOwners();
+    }
 
-override public void Execute() {
+    override public void Execute() {
 
-    Chr tar = chrSource.GetEnemyPlayer().GetBlocker();
+        Chr tar = chrSource.GetEnemyPlayer().GetBlocker();
 
-    stackClauses.Push(new Clause() {
-        fExecute = () => {
-            Damage dmgToDeal = new Damage(chrSource, tar, 25, false, true);
+        stackClauses.Push(new Clause() {
+            fExecute = () => {
+                Damage dmgToDeal = new Damage(chrSource, tar, nBaseDmg, false, true);
 
-            ContAbilityEngine.Get().AddExec(new ExecDealDamage() {
-                chrSource = this.chrSource,
-                chrTarget = tar,
-                dmg = dmgToDeal,
+                ContAbilityEngine.Get().AddExec(new ExecDealDamage() {
+                    chrSource = this.chrSource,
+                    chrTarget = tar,
+                    dmg = dmgToDeal,
 
-                fDelay = 1.0f,
-                sLabel = tar.sName + " is being stabbed"
-            });
-        }
-    });
+                    fDelay = 1.0f,
+                    sLabel = tar.sName + " is being stabbed"
+                });
+            }
+        });
 
 
-    //NOTE:: Every Execute extension should begin with a typecast and end with a base.Execute call;
+        //NOTE:: Every Execute extension should begin with a typecast and end with a base.Execute call;
 
-    base.Execute();
-}
+        base.Execute();
+    }
 
 }
