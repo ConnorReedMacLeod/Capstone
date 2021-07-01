64478d37b5a5b450e8b8fb53d5f8feced0cd3987
Bani Singh <bani.singh@uwaterloo.ca>
2021-06-30 21:14:15 -0400

    SoulPosition Added
    Added Views for it as well - just need to make some test skills for it

---
+++ Assets/Scripts/Model/Soul/SoulContainerPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Adds in a reference to the Position owning the SoulContainer and implements a few methods that depend on the owner
+public class SoulContainerPosition : SoulContainer {
+
+    public Position posOwner;
+
+
+    public override string GetOwnerName() {
+        return posOwner.ToString();
+    }
+
+    public override void InitMaxVisibleSoul() {
+        nMaxVisibleSoul = 1;
+    }
+
+    public override void LetOwnerNotifySoulApplied(Soul soulApplied) {
+        posOwner.subSoulApplied.NotifyObs(this, soulApplied);
+    }
+
+    public override void LetOwnerNotifySoulRemoved(Soul soulRemoved) {
+        posOwner.subSoulRemoved.NotifyObs(this, soulRemoved);
+    }
+}
---
+++ Assets/Scripts/Model/Soul/SoulPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// All this really needs to do is add a chrTarget field
+public class SoulPosition : Soul {
+
+    public Position posTarget;     //A reference to the Position this soul effect is applied to
+
+    public SoulPosition(Chr _chrSource, Position _posTarget, Skill _skillSource) : base(_chrSource, _skillSource) {
+
+        posTarget = _posTarget;
+
+    }
+
+    public SoulPosition(SoulPosition soulToCopy, Position _posTarget = null) : base(soulToCopy) {
+
+        if(_posTarget != null) {
+            //If a Target was provided, then we'll use that
+            posTarget = _posTarget;
+        } else {
+            //Otherwise, just copy from the other object
+            posTarget = soulToCopy.posTarget;
+        }
+
+    }
+}
