3c0022f9325209d3088bcda827f0eaa03c913d18
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2021-12-20 02:21:58 -0500

    Most of the game processing loop is prepared

--- Assets/Scripts/Model/SkillEngine/Executables/Executable.cs
+++ Assets/Scripts/Model/SkillEngine/Executables/Executable.cs
@@ -33,34 +33,41 @@ public abstract class Executable {
     public IEnumerator Execute() {
         if (isLegal() == false) {
             Debug.Log("Executable of type  " + this.GetType().ToString() + " has been cancelled since it's no longer legal");
-
-        } else {
-            //If the executable  is legal, then do its effect
-
-            //Perform all of the effects of the executable
-            ExecuteEffect();
-
-            //Let the AudioManager play the associated sound effect (if there is one)
-            if (arSoundEffects != null && arSoundEffects.Length != 0) {
-                float fPlayTime = AudioManager.Get().PlaySoundEffect(arSoundEffects);
-                //Debug.Log("fPlayTime is " + fPlayTime + " for " + this.sLabel);
-            }
-
-            //Put our post-trigger effects onto the stack so they'll be executed next
-            GetPostTrigger().NotifyObs(null, this);
-
+            yield break;
         }
 
-        //Delay for the appropriate amount of time before 
-        if(fDelay > 0) {
-            yield return new WaitForSeconds(fDelay);
-        }
+        //If the executable  is legal, then do its effect
 
-        //Now that we've done our thing, let the engine know to start processing the next thing
-        ContSkillEngine.Get().InvokeProcessStack(fDelay, sLabel, bStopAutoProcessing);
+        //Perform all of the effects of the executable
+        ExecuteEffect();
+
+        //Put our post-trigger effects onto the stack so they'll be executed next
+        GetPostTrigger().NotifyObs(null, this);
+
+        //Can extend either of these as needed
+        PlaySoundEffects();
+        yield return PlayAnimations(); 
     }
 
     public abstract void ExecuteEffect();
+
+    public virtual void PlaySoundEffects() {
+        //Let the AudioManager play the associated sound effect (if there is one)
+        if (arSoundEffects != null && arSoundEffects.Length != 0) {
+            float fPlayTime = AudioManager.Get().PlaySoundEffect(arSoundEffects);
+            //Debug.Log("fPlayTime is " + fPlayTime + " for " + this.sLabel);
+        }
+    }
+
+    public virtual IEnumerator PlayAnimations() {
+        if (fDelay == 0f) yield break;
+
+        //By default, we'll just start a timer with an appropriate label, then wait for it to finish
+
+        ContSkillEngine.Get().SpawnTimer(fDelay, sLabel);
+
+        yield return new WaitForSeconds(fDelay);
+    }
 
     public Executable(Chr _chrSource) {
         chrSource = _chrSource;
--- Assets/Scripts/Model/SkillEngine/MatchInput.cs
+++ Assets/Scripts/Model/SkillEngine/MatchInput.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public abstract class MatchInput {
 
+    //Store which player is responsible for filling in and executing this input
+    public int iPlayerActing;
+
+    //Each input type should extend this to provide some process by which the MatchInput information is filled out
+    public abstract IEnumerator GatherManualInput();
+
+    //Each input type should extend this to define what this input should actually do to affect the game state
     public abstract IEnumerator Execute();
 
+    public MatchInput(int _iPlayerActing) {
+        iPlayerActing = _iPlayerActing;
+    }
+
+    public MatchInput(MatchInput other) {
+        iPlayerActing = other.iPlayerActing;
+    }
 }
