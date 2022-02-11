3494804509a9866e63ac1b1bdb764a4bf37a8888
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2022-02-11 14:00:17 -0500

    Added Colored Debugging Messages

---
+++ Assets/Scripts/Library/LibDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class LibDebug {
+    
+    public enum Col { BLACK, RED, BLUE, GREEN, MAGENTA };
+    public static readonly string[] ColNames = new string[] { "black", "red", "blue", "green", "magenta" };
+    
+    public static void LogWithColor(string sText, Col col, params object[] args) {
+        Debug.LogFormat(string.Format("<color={0}>{1}</color>", ColNames[(int)col], sText), args);
+    }
+
+    //TODO - add in file logging
+}
