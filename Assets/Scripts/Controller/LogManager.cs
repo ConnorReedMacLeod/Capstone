2016e815ac6944139419926846b039ff171f1eca
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2022-02-18 04:36:30 -0500

    First Pass of Logging Complete
    Many things to fix with the logging process though

--- Assets/Scripts/Controller/LogManager.cs
+++ Assets/Scripts/Controller/LogManager.cs
@@ -14,16 +14,17 @@ public class LogManager : SingletonPersistent<LogManager> {
     public const int nMAXLOGFILES = 5;
 
     public override void Init() {
+        
+    }
+
+    public void InitMatchLog() {
 
         CleanOldLogFiles();
 
-        InitLogFilePath();
+        CreateMatchLogFile();
 
-        PrintRandomSeed();
+        LogMatchSetup();
 
-        //Subscribe to all events that we support log printing for
-        Chr.subAllDeath.Subscribe(cbCharacterDied);
-        Player.subAllPlayerLost.Subscribe(cbPlayerLost);
     }
 
     public void CleanOldLogFiles() {
@@ -34,49 +35,264 @@ public class LogManager : SingletonPersistent<LogManager> {
 
     }
 
-    public void InitLogFilePath() {
+    public string GetDateTime() {
+
+        System.DateTimeOffset timeCur = new System.DateTimeOffset(System.DateTime.UtcNow).ToLocalTime();
+
+        return string.Format("{0}-{1}-{2}-{3}-{4}", timeCur.Year, timeCur.Month, timeCur.Day, timeCur.Hour, timeCur.Minute);
+    }
+
+    public string GetTimestamp() {
+
+        System.DateTimeOffset timeCur = new System.DateTimeOffset(System.DateTime.UtcNow).ToLocalTime();
+
+        return string.Format("{0}-{1}", timeCur.Minute, timeCur.Second);
+    }
+
+    public void CreateMatchLogFile() {
 
         //Initialize the file name we'll want for the log for this match
         timeStart = new System.DateTimeOffset(System.DateTime.UtcNow).ToLocalTime();
         Debug.Log("Current time is " + timeStart.ToString());
 
-        sLogfilePath = sLOGSPATH + "log-" + timeStart.Year + "-" + timeStart.Month + "-" + timeStart.Day + "-" + timeStart.Hour + "-" + timeStart.Minute + ".txt";
+        sLogfilePath = string.Format("{0}log-{1}.txt", sLOGSPATH, GetDateTime());
 
         //Delete the file if it already exists
         if(File.Exists(sLogfilePath)) File.Delete(sLogfilePath);
 
-        Debug.Log(sLogfilePath);
+        Debug.LogFormat("Creating {0}", sLogfilePath);
 
         //Initialize the writer
         swFileWriter = new StreamWriter(sLogfilePath, false);
 
     }
 
-    public void PrintRandomSeed() {
-        WriteLogFile(Random.state.ToString());
+    public void CloseLogFile() {
+
+        WriteToMatchLogFile("Log Complete");
+
+        swFileWriter.Close();
+    }
+
+    public void LogMatchSetup() {
+
+        LogPlayerOwners();
+        LogInputTypes();
+        LogCharacterSelections(0);
+        LogCharacterSelections(1);
+        LogLoadouts(0);
+        LogLoadouts(1);
+        LogPositionCoords(0);
+        LogPositionCoords(1);
+        LogRandomizationSeed();
+
+    }
+
+    public void LoadLoggedMatchSetup(string sLogFilePath) {
+
+        string[] arsLogLines = File.ReadAllLines(sLogFilePath);
+
+        LoadLoggedPlayerOwners(arsLogLines[0].Split(':'));
+        LoadLoggedInputTypes(arsLogLines[1].Split(':'));
+        LoadLoggedCharacterSelections(arsLogLines[2].Split(':'));
+        LoadLoggedCharacterSelections(arsLogLines[3].Split(':'));
+        LoadLoggedLoadouts(arsLogLines[4].Split(':'));
+        LoadLoggedLoadouts(arsLogLines[5].Split(':'));
+        LoadLoggedPositionCoords(arsLogLines[6].Split(':'));
+        LoadLoggedPositionCoords(arsLogLines[7].Split(':'));
+        LoadLoggedRandomizationKey(arsLogLines[8].Split(':'));
+
+    }
+
+    public void LogPlayerOwners() {
+        WriteToMatchLogFile(string.Format("po:{0}:{1}", NetworkMatchSetup.GetPlayerOwner(0), NetworkMatchSetup.GetPlayerOwner(1)));
+    }
+
+    public void LoadLoggedPlayerOwners(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "po");
+
+        int playerowner0, playerowner1;
+
+        if (int.TryParse(arsSplitLogs[1], out playerowner0) == false || playerowner0 < 0) {
+            Debug.LogErrorFormat("Error! {0} was not a valid player owner to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[2], out playerowner1) == false || playerowner1 < 0) {
+            Debug.LogErrorFormat("Error! {0} was not a valid player owner to be loaded", arsSplitLogs[2]);
+            return;
+        }
+
+        NetworkMatchSetup.SetPlayerOwner(0, playerowner0);
+        NetworkMatchSetup.SetPlayerOwner(1, playerowner1);
+    }
+
+    public void LogInputTypes() {
+        WriteToMatchLogFile(string.Format("it:{0}:{1}", NetworkMatchSetup.GetInputType(0), NetworkMatchSetup.GetInputType(1)));
+    }
+
+    public void LoadLoggedInputTypes(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "it");
+
+        int inputtype0, inputtype1;
+
+        if (int.TryParse(arsSplitLogs[1], out inputtype0) == false || inputtype0 < 0) {
+            Debug.LogErrorFormat("Error! {0} was not a valid input type to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[2], out inputtype1) == false || inputtype1 < 0) {
+            Debug.LogErrorFormat("Error! {0} was not a valid input type to be loaded", arsSplitLogs[2]);
+            return;
+        }
+
+        NetworkMatchSetup.SetInputType(0, (Player.InputType)inputtype0);
+        NetworkMatchSetup.SetInputType(1, (Player.InputType)inputtype1);
+    }
+
+    public void LogCharacterSelections(int iPlayer) {
+
+        for(int iChr = 0; iChr < Player.MAXCHRS; iChr++) {
+            CharType.CHARTYPE chartype = NetworkMatchSetup.GetCharacterSelection(iPlayer, iChr);
+
+            WriteToMatchLogFile(string.Format("cs:{0}-{1}:{2}:{3}", iPlayer, iChr, chartype, CharType.GetChrName(chartype)));
+        }
+    }
+
+    public void LoadLoggedCharacterSelections(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "cs");
+
+        int iPlayer, iChr, nSerializedCoords;
+
+        if (int.TryParse(arsSplitLogs[1], out iPlayer) == false || iPlayer < 0 || iPlayer >= Player.MAXPLAYERS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid player id to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[2], out iChr) == false || iChr < 0 || iChr >= Player.MAXCHRS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid chr id to be loaded", arsSplitLogs[2]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[3], out nSerializedCoords) == false) {
+            Debug.LogErrorFormat("Error! {0} was not a valid serialized character selections to be loaded", arsSplitLogs[3]);
+            return;
+        }
+
+        NetworkMatchSetup.SetPositionCoords(iPlayer, iChr, Position.UnserializeCoords(nSerializedCoords));
+    }
+
+    public void LogLoadouts(int iPlayer) {
+        
+        for(int iChr = 0; iChr < Player.MAXCHRS; iChr++) {
+            LoadoutManager.Loadout loadout = NetworkMatchSetup.GetLoadout(iPlayer, iChr);
+
+            string sLoadout = string.Format("lo:{0}-{1}", iPlayer, iChr);
+
+            int[] arnSerializedLoadout = LoadoutManager.SerializeLoadout(loadout);
+
+            //For each entry of our loadout, add it to our string to log
+            for(int i=0; i<arnSerializedLoadout.Length; i++) {
+                sLoadout += ":" + arnSerializedLoadout[i].ToString();
+            }
+
+            sLoadout += ":" + loadout;
+
+            WriteToMatchLogFile(sLoadout);
+        }
+
+    }
+
+    public void LoadLoggedLoadouts(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "lo");
+
+        int iPlayer, iChr;                                                                  
+
+        if (int.TryParse(arsSplitLogs[1], out iPlayer) == false || iPlayer < 0 || iPlayer >= Player.MAXPLAYERS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid player id to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[2], out iChr) == false || iChr < 0 || iChr >= Player.MAXCHRS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid chr id to be loaded", arsSplitLogs[2]);
+            return;
+        }
+
+        int[] arnSerializedLoadout = new int[arsSplitLogs.Length - 3];
+
+        //Copy and translate all the logged strings into serialized ints in an array
+        for(int i=0; i<arnSerializedLoadout.Length; i++) {
+            if (int.TryParse(arsSplitLogs[i+3], out arnSerializedLoadout[i]) == false) {
+                Debug.LogErrorFormat("Error! {0} was not a valid serialized loadout entry to be loaded", arsSplitLogs[i+3]);
+                return;
+            }
+        }
+
+        NetworkMatchSetup.SetLoadout(iPlayer, iChr, LoadoutManager.UnserializeLoadout(arnSerializedLoadout));
+    }
+
+    public void LogPositionCoords(int iPlayer) {
+
+        for(int iChr=0; iChr < Player.MAXCHRS; iChr++) {
+            Position.Coords poscoords = NetworkMatchSetup.GetPositionCoords(iPlayer, iChr);
+
+            WriteToMatchLogFile(string.Format("pc:{0}-{1}:{2}:{3}", iPlayer, iChr, Position.SerializeCoords(poscoords), poscoords));
+        }
+    }
+
+    public void LoadLoggedPositionCoords(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "pc");
+
+        int iPlayer, iChr, nSerializedCoords;
+
+        if (int.TryParse(arsSplitLogs[1], out iPlayer) == false || iPlayer < 0 || iPlayer >= Player.MAXPLAYERS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid player id to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[2], out iChr) == false || iChr < 0 || iChr >= Player.MAXCHRS) {
+            Debug.LogErrorFormat("Error! {0} was not a valid chr id to be loaded", arsSplitLogs[2]);
+            return;
+        }
+        if (int.TryParse(arsSplitLogs[3], out nSerializedCoords) == false) {
+            Debug.LogErrorFormat("Error! {0} was not a valid serialized coordinates to be loaded", arsSplitLogs[3]);
+            return;
+        }
+
+        NetworkMatchSetup.SetPositionCoords(iPlayer, iChr, Position.UnserializeCoords(nSerializedCoords));
+    }
+
+    public void LogRandomizationSeed() {
+        WriteToMatchLogFile(string.Format("rs:{0}", NetworkMatchSetup.GetRandomizationSeed().ToString()));
+    }
+
+    public void LoadLoggedRandomizationKey(string[] arsSplitLogs) {
+
+        Debug.Assert(arsSplitLogs[0] == "rs");
+
+        int nRandomizationKey;
+
+        if (int.TryParse(arsSplitLogs[1], out nRandomizationKey) == false) {
+            Debug.LogErrorFormat("Error! {0} was not a valid randomization key to be loaded", arsSplitLogs[1]);
+            return;
+        }
+        NetworkMatchSetup.SetRandomizationSeed(nRandomizationKey);
+    }
+
+    public void LogMatchInput() {
+        MatchInput curinput = NetworkMatchReceiver.Get().GetCurMatchInput();
+        WriteToMatchLogFile(string.Format("mi:{0}:{1}:{2}", NetworkMatchReceiver.Get().indexCurMatchInput, curinput.Serialize(), curinput));
     }
 
     public void OnApplicationQuit() {
 
-        WriteLogFile("ApplicationQuit");
+        WriteToMatchLogFile("ApplicationQuit");
 
-        //Close the writer
-        swFileWriter.Close();
-
-    }
-
-
-    public void cbPlayerLost(Object target, params object[] args) {
-
+        CloseLogFile();
 
     }
+    
 
-
-    public void cbCharacterDied(Object target, params object[] args) {
-
-    }
-
-    public void WriteLogFile(string sText) {
+    public void WriteToMatchLogFile(string sText, bool bTimestamp = false) {
 
         swFileWriter.WriteLine(sText);
 
