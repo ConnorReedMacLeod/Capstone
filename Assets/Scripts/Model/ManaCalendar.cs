632d8a9edd8e8763af11e7e7d8f33c24affdaeaa
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2022-03-18 04:25:48 -0400

    Base Mana Calendar Setup Done
    Mana Calendar classes set up and will give proper starting mana at the beginning of each turn

---
+++ Assets/Scripts/Model/ManaCalendar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Tracks the mana gain scheduled for a player at the beginning of each turn
+public class ManaCalendar : MonoBehaviour {
+
+    public const int NDAYS = 12;
+
+    public Player plyrOwner;
+
+    public class ManaDate {
+        public ManaCalendar manacalendar;
+
+        public int nDay;
+
+        public int nRandomManaToGain;
+        public Property<Mana> pmanaToGain;
+
+        public ManaDate(ManaCalendar _manaCalendar, int _nDay, int _nRandomManaToGain) {
+            manacalendar = _manaCalendar;
+            nDay = _nDay;
+            nRandomManaToGain = _nRandomManaToGain;
+            pmanaToGain = new Property<Mana>(new Mana(0, 0, 0, 0));
+        }
+
+    }
+
+    public ManaDate[] arManaDates;
+
+    public void InitManaDays() {
+
+        arManaDates = new ManaDate[NDAYS];
+
+        for(int i=0; i<NDAYS; i++) {
+            arManaDates[i] = new ManaDate(this, i, 1);
+        }
+
+    }
+
+    public ManaDate GetCurrentManaDay() {
+
+        int nCurDay = ContTurns.Get().nTurnNumber % NDAYS;
+
+        return arManaDates[nCurDay];
+
+    }
+
+
+}
