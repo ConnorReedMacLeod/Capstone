b4b7677d4846a9fb22ef23db472ebb9c518c0770
Ben-Hicks <31557370+Ben-Hicks@users.noreply.github.com>
2022-03-23 21:47:55 -0400

    Mana Calendar Functioning
    Gives mana and displays properly.  Still need to add hotkeys to view/hide the calendars, and make some test skills that affect mana added on the calendar

--- Assets/Scripts/View/ManaUI/ViewManaCalendar.cs
+++ Assets/Scripts/View/ManaUI/ViewManaCalendar.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class ViewManaCalendar : MonoBehaviour {
 
     public ViewManaDate[] arViewManaDate;
+    public Text txtTitle;
 
     public ManaCalendar modManaCalendar;
     
     // Start is called before the first frame update
     public void Start() {
+        
+        txtTitle.text = string.Format("Mana Calendar for Player {0}", modManaCalendar.plyrOwner.id);
 
     }
     
