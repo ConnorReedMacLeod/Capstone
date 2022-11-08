a883766b5ac38d8720c82ce41c25b62a70c9a66c
Bani Singh <bani.singh@uwaterloo.ca>
2022-11-08 07:10:30 -0500

    Added Timestamps for Deaths
    And generally added a timestamp system for tracking when executables have been executed

--- Assets/Scripts/Controller/ContDeaths.cs
+++ Assets/Scripts/Controller/ContDeaths.cs
@@ -6,41 +6,58 @@ using System.Linq;
 public class ContDeaths : Singleton<ContDeaths> {
 
 
-    public Queue<Chr> queueDyingChrs; //Tracks a list of all characters that have been flagged as dying (in order of their death 'timestamp')
+    public struct DeathTimestamp {
+        public Timestamp timestampDeath;
+        public Chr chr;
+
+        public DeathTimestamp(Timestamp _timestampDeath, Chr _chr) {
+            timestampDeath = _timestampDeath;
+            chr = _chr;
+        }
+    }
+
+    public Queue<DeathTimestamp> queueDyingChrs; //Tracks a list of all characters that have been flagged as dying (in order of their death 'timestamp')
 
 
     public override void Init() {
-        queueDyingChrs = new Queue<Chr>();
+        queueDyingChrs = new Queue<DeathTimestamp>();
     }
 
 
     public void AddDyingChr(Chr chr) {
 
-        if(queueDyingChrs.Contains(chr)) {
-            Debug.LogFormat("{0} is already earlier in the queue of dying characters", chr);
-            return;
-        }
+        Timestamp curTimestamp = ContTimestamp.Get().GetCurrentTimestamp();
 
-        queueDyingChrs.Enqueue(chr);
+        //Record the death of this chr with the current timestamp at the time of death
+        //  - we'll use this timestamp to later determine if 
+        queueDyingChrs.Enqueue(new DeathTimestamp(curTimestamp, chr));
+
+        //Store this death timestamp in the dead character themselves so they know at
+        // what time they died
+        chr.timestampDeath = curTimestamp;
     }
-
 
     //Find the first character that has been flagged as dying and that actually should die and push a death effect to transition them to a dead state
     // returns true/false if there are/aren't any dead characters
     public bool KillNextFlaggedDyingCharacter() {
 
         while(queueDyingChrs.Count != 0) {
-            Chr chrNextFlagged = queueDyingChrs.Dequeue();
+            DeathTimestamp chrNextFlaggedDyingTimestamp = queueDyingChrs.Dequeue();
 
             //If this first character is indeed supposed to die
-            if(chrNextFlagged.IsDying()) {
+            // (and we're also double-checking that this timestamp we're checking is indeed the most recent death
+            //   the character has faced - i.e., if the character dropped below 0, then went above 0, then dropped below 0 again,
+            //   then we want to make sure we're only processing the second death to ensure the order of execution is correct)
+            if(chrNextFlaggedDyingTimestamp.chr.IsDying()
+                && chrNextFlaggedDyingTimestamp.chr.timestampDeath.nTimestamp == chrNextFlaggedDyingTimestamp.timestampDeath.nTimestamp) {
                 //Then let's push a death executable onto the stack to kill the character
 
-                ContSkillEngine.PushSingleExecutable(new ExecKillFlaggedDyingChr(null, chrNextFlagged));
+                ContSkillEngine.PushSingleExecutable(new ExecKillFlaggedDyingChr(null, chrNextFlaggedDyingTimestamp.chr));
 
                 return true; //Return true that we have indeed found a character to kill
             }
         }
+        //We can guarantee that our queue will be completely cleared out if we're reached this point
 
         //If we've exited the loop, then we didn't find any characters that were supposed to die, so just return false
         return false;
---
+++ Assets/Scripts/Model/SkillEngine/Timestamp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timestamp {
+
+    public int nTimestamp;
+    public Executable exec;
+
+    public Timestamp(int _nTimestamp, Executable _exec) {
+        nTimestamp = _nTimestamp;
+        exec = _exec;
+    }
+
+}
